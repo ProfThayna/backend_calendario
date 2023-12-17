3a762b864cb0e5696373592a337f4e4ca7b3e99f
Thayná S. de Matos <89859922+ProfThayna@users.noreply.github.com>
2023-12-17 18:02:26 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// available slots
+const slots = {
+  '2023-12-17': ['08:00', '09:00', '10:00','14:00','15:00'],
+  '2023-12-18': ['11:00', '12:00', '13:00','14:00','15:00'],
+  '2023-12-19': ['11:00', '12:00', '13:00','14:00','15:00'],
+  '2023-12-20': ['11:00', '12:00', '13:00','14:00','15:00'],
+  '2023-12-21': ['11:00', '12:00', '13:00','14:00','15:00'],
+  '2023-12-22': ['11:00', '12:00', '13:00','14:00','15:00'],
+
+  //pode ser adicionado mais slots caso necessario
+};
+
+app.get('/slots', (req, res) => {
+  res.json(slots);
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
